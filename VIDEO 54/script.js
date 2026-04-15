269f28cb48761d3ee701b52d8bdc60bcda8d1bb9
MD. Jul Ikram Shams <mr.remian7@gmail.com>
2026-04-15 23:40:55 +0600

    Video 1-55 Done

---
+++ VIDEO 54/script.js
@@ -0,0 +1,24 @@
+alert("Hello World");
+
+console.log("Code is running...");
+console.log("Code is also running...");
+console.log("Code is looking like a wow...");
+
+var a = prompt("Enter a number");
+var isNihdi = confirm("Are you sure you want to leave this page and blast your computer?");
+
+if(isNihdi){
+    // console.log("Computer is blasting...");
+    alert("Computer is blasting...");
+}
+
+else{
+    // console.log("Computer is not blasting");
+    alert("Computer is not blasting");
+}
+
+console.log("The number you entered is " + a);
+
+document.title = "JavaScript Introduction";
+
+// document.body.style.backgroundColor = "red"
---
+++ VIDEO 55/script.js
@@ -0,0 +1,46 @@
+console.log("Shams weds Nidhi");
+console.log("Hey, this is tutorial 55");
+
+a = 10;
+var b = 3;
+var c = "Nidhi"
+var _a = 12;
+//var 55a = 12; // Not allowed as variable name cannot start with a number
+
+console.log("The value of a + b is " + (a + b));
+console.log(a + b + 8);
+// console.log(a = a + 1);
+console.log(typeof a, typeof b, typeof c);
+
+const  a1 = 420;
+// a1 = a1 + 1; Not allowed as a1 is a constant variable
+// console.log(a1 = a1 + 1); Not allowed as a1 is a constant variable
+
+
+let A = 100
+{
+    let A = 200
+    console.log(A);
+}
+
+console.log(A);
+
+let x = "Harry Bhai";
+let y = 22;
+let z = 3.14;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+let o = {
+    name: "Nidhi",
+    "job role": "Wife of Shams",
+    "job code": 420,
+    is_beautiful: true
+    }
+console.log(o);
+o.salary = "100crores";
+console.log(o);
