3cf1c4199612bc31a574b5cdf5c2034f107cdc9c
MD. Jul Ikram Shams <mr.remian7@gmail.com>
2026-04-25 17:27:04 +0600

    Video 57 Done

---
+++ VIDEO 57/index.js
@@ -0,0 +1,47 @@
+// console.log("I am a tutorial on Loops");
+
+// let a = 1;
+
+// console.log(a)
+// console.log(a+1)
+// console.log(a+2)
+// console.log(a+3)
+
+// for (let i = 0; i < 100; i++) {
+//     console.log(a + i);
+// }
+
+// let Shams = "I love U NIDHIIIII"
+
+// for(let i = 0; i<100; i++){
+//     console.log(i + "." + Shams);
+// }
+
+// let Nidhi = {
+//     name: "Ayesha Hossain Nidhi",
+//     soulmate_name: "Jul Ikram Shams",
+//     job: "Wife of Shams",
+//     have_kids: "No"
+// }
+
+// for (const key in Nidhi) {    
+//     // const element = Nidhi[key];
+//     console.log(key)
+// }
+
+// for (const element of "Nidhi") {
+//     console.log(element)
+// }
+
+
+// let i = 0;
+// while (i < 6) {
+//     console.log(i);
+//     i++;
+// }
+
+let i = 10;
+do {
+    console.log(i);
+    i++;
+} while (i < 6);
